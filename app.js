0be069bf9012d85f533d132d324637d2a4713cc2
Kalia-Lee Rodney <kaliarodney@gmail.com>
2020-11-06 09:48:37 -0500

    Exercise 2completed

--- app.js
+++ app.js
@@ -4,33 +4,27 @@ window.onload = function(){
     
     searchbtn.addEventListener('click' , function(e) {
         e.preventDefault();
-        hrequest = new XMLHttpRequest();
+        var hrequest = new XMLHttpRequest();
 
         var urlcode = "superheroes.php";
-        var handleChange = function() {
-            if (hrequest.readyState == XMLHttpRequest.DONE && hrequest.status == 200){
-                var hero = hrequest.responseText;
-                var list = document.querySelector("#list");
-                list.innerHTML = hero;
-                alert(hero);
+
+        hrequest.onreadystatechange = function() {
+            if (hrequest.readyState == XMLHttpRequest.DONE){
+                if (hrequest.status == 200) {
+                    var hero = hrequest.responseText;
+                    /*var list = document.querySelector("#heroes");*/
+                    /*list.innerHTML = hero;*/
+                    alert(hero);
+                } else{
+                    alert("Error Detected");
+                }
 
             }
-
-            else{
-                alert("Error Detected");
-            }
-
         }
+
         hrequest.open("GET" , urlcode, true);
-        hrequest.onreadystatechange = handleChange;
         hrequest.send();
     });
 
-  
-
-
-
-    
-
-
 }
+
